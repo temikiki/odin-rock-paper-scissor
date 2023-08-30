de10d754aee58b4a3772c597379f1601cf01f10c
temikiki <temitayokiki@gmail.com>
2023-08-30 12:04:11 +0100

    updated the ui of the page

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+//let computer;
+
+//player = prompt("rock, paper, scissor, pick one:");
+//getcomputerchoice();
+
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const btns = document.querySelectorAll(".btn")
+let player;
 let computer;
-player = prompt("rock, paper, scissor, pick one:");
-getcomputerchoice();
+let result;
 
 
+btns.forEach(button => button.addEventListener("click", ()=>{
+    player = button.textContent;
+    getcomputerchoice();
+    playerText.textContent = `player: ${player}`
+    computerText.textContent = `computer: ${computer}`;
+    resultText.textContent = gamerules();
+    result = `result: ${result}`
+})); 
 function getcomputerchoice(){
-    computerchoice = Math.floor(Math.random() * 3) + 1;
+   const computerchoice = Math.floor(Math.random() * 3) + 1;
     
     switch(computerchoice){
         case 1:
@@ -21,31 +38,40 @@ function getcomputerchoice(){
 
 
 
-console.log("computer's choice:" + computer);
-console.log("palyer's choice:" + player)
+console.log("computer's  choice:" + computer);
+console.log("player's choice:" + player)
 
 function gamerules(){
     if (computer == player){
         console.log("Draw");
+        return("draw")
     }
     else if (computer == "rock" && player == "scissor"){
         console.log("computer wins, player loses")
+        return("computer wins, player loses")
     }
     else if (computer == "scissor" && player == "paper"){
         console.log("computer wins, player loses")
+        return("computer wins, player loses")
     }
     else if (computer == "paper" && player == "rock"){
         console.log("computer wins, player loses")
+        return("computer wins, player loses")
+
     }
     else if (player == "rock" && computer == "scissor"){
         console.log("player wins, computer loses")
+        return("player wins, computer loses")
+    }
+    else if (player == "scissor" && computer == "paper"){
+        console.log("player wins, computer loses")
+        return("player wins, computer loses")
     }
     else if (player == "paper" && computer == "rock"){
         console.log("player wins, computer loses")
+        return("player wins, computer loses")
     }
-    else if (player == "scissor" && computer == "paper "){
-        console.log("player wins, computer loses")
-    }
+    
 }
 gamerules();
 //console.log("hello")
